17cbd8a8630bb59ecc70f632e0564feb74e38904
freitzzz <1160907@isep.ipp.pt>
2022-08-17 12:32:11 +0100

    init dart console app

---
+++ bin/randomize_photo_mosh.dart
@@ -0,0 +1,5 @@
+import 'package:randomize_photo_mosh/randomize_photo_mosh.dart' as randomize_photo_mosh;
+
+void main(List<String> arguments) {
+  print('Hello world: ${randomize_photo_mosh.calculate()}!');
+}
---
+++ test/randomize_photo_mosh_test.dart
@@ -0,0 +1,8 @@
+import 'package:randomize_photo_mosh/randomize_photo_mosh.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
